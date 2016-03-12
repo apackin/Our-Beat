8d78bb5ae07d219e8611886c472d149d606f08e6
apackin <apackin@gmail.com>
2016-03-12 16:19:11 -0500

    Added volume ctrl

--- browser/js/directives/directive.sequencer.js
+++ browser/js/directives/directive.sequencer.js
@@ -29,11 +29,12 @@ angular.module('myBeatApp').directive('header', function() {
         	var matId = 'matrix' + scope.part;
         	var noteOptions = window['selected'+scope.part+'Notes'].slice();
         	scope.noteOptions = noteOptions;
-
+            scope.currentVolume = 1;
         	scope.notesInputs = [];
 
-        	setTimeout(numberOfRows, 500);
 
+            // can only check number of rows after they are drawn
+        	setTimeout(numberOfRows, 500);
         	scope.selectedANote = function (idx, note) {
         		window['selected'+scope.part+'Notes'][idx] = note;
         	}
@@ -41,21 +42,6 @@ angular.module('myBeatApp').directive('header', function() {
         	scope.selectedAnOption = function (idx, opt) {
         		window['selected'+scope.part+'Options'][idx] = opt;
         	}
-
-     		function numberOfRows () {
-     			for (var i = 0; i < window[matId].row; i++) {
-     				addSelector();
-     			}
-     			scope.$digest();
-     		}
-
-     		function addSelector() {
-     			scope.notesInputs.push('A1');
-     		}
-
-     		function removeSelector() {
-     			scope.notesInputs.pop('A1');
-     		}
 
 	        scope.addRow = function () {
 		        window[matId].row++;
@@ -69,11 +55,34 @@ angular.module('myBeatApp').directive('header', function() {
                 setMatrixSize(window[matId]);
 		    }
 
+            scope.updateVolume = function () {
+                console.log(window[scope.part+'Synth']);
+                window[scope.part+'Synth'].volume.input.value = scope.currentVolume;
+            }
+
+            function numberOfRows () {
+                for (var i = 0; i < window[matId].row; i++) {
+                    addSelector();
+                }
+                scope.$digest();
+            }
+
+            function addSelector() {
+                scope.notesInputs.push('A1');
+                window['selected'+scope.part+'Notes'].push(scope.noteOptions[3]);
+                window['selected'+scope.part+'Options'].push(3);
+            }
+
+            function removeSelector() {
+                scope.notesInputs.pop('A1');
+            }
+
             function setMatrixSize (matrix) {
                 var matHeight = (matrix.row*35)
                 matrix.resize($("#Content").width()/1.3, matHeight);
                 matrix.draw();
             }
+
 
 
         }
